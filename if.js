a5f04831f5a58564b00a3ee43055def29bf25a32
gssganesh <161111668+gssganesh@users.noreply.github.com>
2024-07-16 15:16:28 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,10 @@
+console.log("hey guys");
+ const readlinesync= require("readlinesync");
+ const userage=readlinesync.question("what is your age? ");
+
+ if(userage > 18)
+ {
+    console.log("i kown myan your an Adult");
+ }else{
+    console.log("your are not pedhamanishii");
+ }
---
+++ ifelse.js
@@ -0,0 +1,11 @@
+console.log("hey guys");
+ const readlinesync= require("readlinesync");
+ const userage=readlinesync.question("what is your age? ");
+
+ if(userage > 18)
+ {
+    console.log("i kown myan your an Adult");
+ }else{
+    console.log("your are not pedhamanishii");
+ }
+ 
